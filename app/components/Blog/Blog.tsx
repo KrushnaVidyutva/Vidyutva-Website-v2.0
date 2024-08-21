b3fb46539c645e57e21db476025ce68f0bd6c27d
SujitVidyutva <sujit.maharana@vidyutva.com>
2024-08-21 19:37:27 +0530

    Update blog section with with latest news from vidyutva with linkedin feeds

--- app/components/Blog/Blog.tsx
+++ app/components/Blog/Blog.tsx
@@ -21,27 +21,54 @@ const Blog = () => {
                 <div className="min-[845px]:grid min-[845px]:grid-cols-3 max-[845px]:flex max-[845px]:flex-col gap-8">
                     <div className="rounded-[1.5rem] p-5 bg-[#d9d9d99c] col-span-1 flex flex-col gap-6">
                         <div className="w-[100%] max-h-[12rem] rounded-[1.25rem] overflow-hidden">
-                            <img src="/images/blog/blog-01.jpg" alt="Loading..." className="transition-all duration-300 ease-in-out imgTrns" />
+                            <img src="/images/blog/Blog1.jpg" alt="Loading..." className="transition-all duration-300 ease-in-out imgTrns" />
                         </div>
-                        <p className="font-medium text-sm text-gray-700">7 min read</p>
-                        <p className="font-medium text-2xl text-gray-900">Breaking the queue: Solutions for a better charging experience</p>
-                        <p className="font-medium text-sm text-gray-700">Product Manager Matt takes a deep dive into queuing for a charge. What really causes it? What are some of the myths? And what we can do about it?</p>
+                        <div className="flex items-end flex-wrap gap-4">
+                            <Link to="https://www.linkedin.com/posts/vidyutva_vidyutva-iitbhubaneswar-evcharging-activity-7217740610923823104-_fSd?utm_source=share&utm_medium=member_android" target="blank" className="flex justify-center items-center text-zinc-800 text-center px-5 py-2 gap-3 rounded-[3.125rem] border border-black bg-white hover:bg-[#00a181] hover:text-white hover:border-white transition-all duration-300 ease-out">
+                                <div className="font-normal text-base pb-1">Read more</div>
+                                <div className="flex justify-center items-center flex-col w-4 h-4">
+                                    <svg width="currentwidth" height="currentheight" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2.586 13L17.172 13L11.879 18.293L13.293 19.707L21 12L13.293 4.29303L11.879 5.70703L17.172 11L2.586 11V13Z" fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </Link>
+                        </div>
+                        <p className="font-medium text-2xl text-gray-900">Vidyutva is now incubated at IIT Bhubaneswar Research & Entrepreneurship Park!</p>
+                        <p className="font-medium text-sm text-gray-700">This milestone marks a significant step forward in our journey to make EV charging user-friendly and reliable in India. </p>
                     </div>
                     <div className="rounded-[1.5rem] p-5 bg-[#d9d9d99c] col-span-1 flex flex-col gap-6">
                         <div className="w-[100%] max-h-[12rem] rounded-[1.25rem] overflow-hidden">
-                            <img src="/images/blog/blog-02.jpg" alt="Loading..." className="transition-all duration-300 ease-in-out imgTrns" />
+                            <img src="/images/blog/Blog2.jpg" alt="Loading..." className="transition-all duration-300 ease-in-out imgTrns" />
                         </div>
-                        <p className="font-medium text-sm text-gray-700">7 min read</p>
-                        <p className="font-medium text-2xl text-gray-900">Breaking the queue: Solutions for a better charging experience</p>
-                        <p className="font-medium text-sm text-gray-700">Product Manager Matt takes a deep dive into queuing for a charge. What really causes it? What are some of the myths? And what we can do about it?</p>
+                        <div className="flex items-end flex-wrap gap-4">
+                            <Link to="https://www.linkedin.com/posts/vidyutva_vidyutva-won-the-2nd-runner-up-mobility-award-activity-7159906491238387712-aYaY?utm_source=share&utm_medium=member_android" target="blank" className="flex justify-center items-center text-zinc-800 text-center px-5 py-2 gap-3 rounded-[3.125rem] border border-black bg-white hover:bg-[#00a181] hover:text-white hover:border-white transition-all duration-300 ease-out">
+                                <div className="font-normal text-base pb-1">Read more</div>
+                                <div className="flex justify-center items-center flex-col w-4 h-4">
+                                    <svg width="currentwidth" height="currentheight" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2.586 13L17.172 13L11.879 18.293L13.293 19.707L21 12L13.293 4.29303L11.879 5.70703L17.172 11L2.586 11V13Z" fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </Link>
+                        </div>
+                        <p className="font-medium text-2xl text-gray-900">Vidyutva won the 2nd runner up Mobility award at IIT Bombay in Eureka competition</p>
+                        <p className="font-medium text-sm text-gray-700">It was a great experience for us meeting 60 amazing startups from 7 different tracks, in front of an amazing lineup of investors at Indian Institute of Technology, Bombay.</p>
                     </div>
                     <div className="rounded-[1.5rem] p-5 bg-[#d9d9d99c] col-span-1 flex flex-col gap-6">
                         <div className="w-[100%] max-h-[12rem] rounded-[1.25rem] overflow-hidden">
-                            <img src="/images/blog/blog-03.jpg" alt="Loading..." className="transition-all duration-300 ease-in-out imgTrns" />
+                            <img src="/images/blog/Blog3.jpg" alt="Loading..." className="transition-all duration-300 ease-in-out imgTrns" />
                         </div>
-                        <p className="font-medium text-sm text-gray-700">7 min read</p>
-                        <p className="font-medium text-2xl text-gray-900">Breaking the queue: Solutions for a better charging experience</p>
-                        <p className="font-medium text-sm text-gray-700">Product Manager Matt takes a deep dive into queuing for a charge. What really causes it? What are some of the myths? And what we can do about it?</p>
+                        <div className="flex items-end flex-wrap gap-4">
+                            <Link to="https://www.linkedin.com/posts/the-founder-institute_founder-institute-gcc-produces-5-new-technology-activity-7224119064623775745-ENvx?utm_source=share&utm_medium=member_desktop" target="blank" className="flex justify-center items-center text-zinc-800 text-center px-5 py-2 gap-3 rounded-[3.125rem] border border-black bg-white hover:bg-[#00a181] hover:text-white hover:border-white transition-all duration-300 ease-out">
+                                <div className="font-normal text-base pb-1">Read more</div>
+                                <div className="flex justify-center items-center flex-col w-4 h-4">
+                                    <svg width="currentwidth" height="currentheight" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2.586 13L17.172 13L11.879 18.293L13.293 19.707L21 12L13.293 4.29303L11.879 5.70703L17.172 11L2.586 11V13Z" fill="currentColor"></path>
+                                    </svg>
+                                </div>
+                            </Link>
+                        </div>
+                        <p className="font-medium text-2xl text-gray-900">Excited to announce Vidyutva's graduation from the Founder Institute GCC program!</p>
+                        <p className="font-medium text-sm text-gray-700">Our team is more energized than ever to take on the challenges ahead and make a lasting impact in the industry...</p>
                     </div>
                 </div>
             </div>
